0d64a84cbc6d72c7c5a294e0cd9a3720a9c08026
Maria Caceres <msierrac@nd.edu>
2020-05-07 23:44:04 -0500

    add more games to arcade

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,2 @@
 console.log('Client-side code running');
 
-/*document.addEventListener('readystatechange', function() {
-    if (document.readyState === "complete") {
-        const button = document.getElementById('yahtzeeLikes');
-        button.addEventListener('click', function(e) {
-            console.log('button was clicked');
-        });
-    }
-});*/
-
